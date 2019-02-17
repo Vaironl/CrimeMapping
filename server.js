66d4ce834247bd1aea98cad7cf3303dbb175032e
Raphael J. Sandor <rsandor@cs.odu.edu>
2019-02-17 11:04:02 -0500

    Created database adapter to handle calls to database.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
 // -----------------------------------------------------
 var express         = require('express');
-const MongoClient   = require("mongodb").MongoClient;
 var port            = process.env.PORT || 3000;
 var morgan          = require('morgan');
 var bodyParser      = require('body-parser');
@@ -24,6 +23,10 @@ app.use(bodyParser.text());                                     // allows bodyPa
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
 app.use(methodOverride());
 
+//Start the database
+require('./public/js/DataAccessAdapter').InitDB();
+
+
 // Routes
 // ------------------------------------------------------
 //require('./app/indexMap.js')(app);
@@ -32,23 +35,4 @@ app.use(methodOverride());
 // -------------------------------------------------------
 app.listen(port);
 console.log('App listening on port ' + port);
-
-const url = "mongodb+srv://Admin:mynameismypassport@cluster0-7xnng.mongodb.net/test?retryWrites=true";
-const dbName = "Cluster0";
-const collectionName = "Crimes";
-var collection;
-
-MongoClient.connect(url, function (err, client) {
-    if (err) {
-        console.log('Error connecting to MongoDB Atlas\n', err);
-    }
-    console.log('Connected...');
-    collection = client.db(dbName).collection(collectionName);
-    crimes = collection.find({}).toArray((error, result) => {
-        if (error) {
-            return result.status(500).send(error);
-        }
-        console.log(result);
-    });
-});
 
