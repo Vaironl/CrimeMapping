8a5c2b07ac97eac81ab118c34b1c8aa8221fdc76
nmendoza <vmend001@odu.edu>
2019-02-18 12:42:29 -0500

    Properly animated sidenav. Needs styling.

--- app/JavaScripts/indexAnimation.js
+++ app/JavaScripts/indexAnimation.js
@@ -1,19 +1,30 @@
 // A $( document ).ready() block.
-$(document).ready(function () {
-    var sidenavHidden = true;
-    $(".sidenav-arrow").click(function () {
-        if (sidenavHidden) {
-            $(".sidenav").animate({width: '20px'}, 350);
-            $(".sidenav-arrow").css({'left':'20px'});
-            $(".sidenav-arrow").html('<h1> < </h1>');
-            sidenavHidden = !sidenavHidden;
-        } else {
-            $(".sidenav").animate({width: '160px'}, 350);
-            $(".sidenav-arrow").css({'left':'155px'},350);
-            $(".sidenav-arrow").css({'opacity':'1'});
-            $(".sidenav-arrow").html('<h1> > </h1>');
-            sidenavHidden = !sidenavHidden;
-        }
-    });
+// $(document).ready(function () {
+//     var sidenavHidden = true;
+//     $(".sidenav-arrow").click(function () {
+//         if (sidenavHidden) {
+//             $(".sidenav").animate({width: '20px'}, 350);
+//             $(".sidenav-arrow").css({'left':'20px'});
+//             $(".sidenav-arrow").html('<h1> < </h1>');
+//             sidenavHidden = !sidenavHidden;
+//         } else {
+//             $(".sidenav").animate({width: '160px'}, 350);
+//             $(".sidenav-arrow").css({'left':'155px'},350);
+//             $(".sidenav-arrow").css({'opacity':'1'});
+//             $(".sidenav-arrow").html('<h1> > </h1>');
+//             sidenavHidden = !sidenavHidden;
+//         }
+//     });
+//
+//
+// });
 
-});
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
