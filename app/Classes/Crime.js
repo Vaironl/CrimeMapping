cce56e453c9f2aa2f70456f32df20e07a6059326
kwatson <kwats001@odu.edu>
2019-02-26 12:26:44 -0500

    Reset branch easy way by copy/pasting Working into KenDeveloper

---
+++ app/Classes/Crime.js
@@ -0,0 +1,61 @@
+
+export default class Crime
+{
+    get id() {
+        return this._id;
+    }
+
+    get crimeCat() {
+        return this._crimeCat;
+    }
+
+    get desc() {
+        return this._desc;
+    }
+
+    get date() {
+        return this._date;
+    }
+
+    get time() {
+        return this._time;
+    }
+
+    get offenses() {
+        return this._offenses;
+    }
+
+    get severity() {
+        return this._severity;
+    }
+
+    get lat() {
+        return this._lat;
+    }
+
+    get lng() {
+        return this._lng;
+    }
+    constructor (
+        _id,
+        crimeCat,
+        desc,
+        date,
+        time,
+        offenses,
+        severity,
+        lat,
+        lng,
+    ) {
+        this._id = _id;
+        this._crimeCat = crimeCat;
+        this._desc = desc;
+        this._date = date;
+        this._time = time;
+        this._offenses = offenses;
+        this._severity = severity;
+        this._lat = lat;
+        this._lng = lng;
+    }
+
+}
