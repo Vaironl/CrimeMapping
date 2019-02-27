f60bc8d07fe9e974ffcaad5d8a0fe30c90e0032b
Kevin Chahine <kchah001@odu.edu>
2019-02-27 11:41:10 -0500

    1st commit in KevinDeveloper

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 
 // Dependencies
+<<<<<<< .merge_file_a20576
 const MongoClient   = require("mongodb").MongoClient;
 
 // Connect to MongoDB Atlas
@@ -22,9 +23,28 @@ MongoClient.connect(url, function (err, client) {
         //console.log(result);
     });
 });
+=======
+var mongoose        = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+var User            = require('./model.js');
+>>>>>>> .merge_file_a14240
 
 // Opens App Routes
 module.exports = function(app) {
+
+    /*// Connect to MongoDB Atlas
+    // -------------------------------------------------------
+    const url =  process.env.DB_CONN;
+    const dbName = "Cluster0";
+    const collectionName = "Crimes";
+
+    MongoClient.connect(url,function(err, client){
+        if(err){
+            console.log('Error connecting to MongoDB Atlas\n',err);
+        }
+        console.log('Connected...');
+        const collection = client.db(dbName).collection(collectionName);
+    });*/
 
     // GET Routes
     // --------------------------------------------------------
@@ -32,6 +52,7 @@ module.exports = function(app) {
     app.get('/crimes', function (req, response) {
 
         // Uses Mongoose schema to run the search (empty conditions)
+<<<<<<< .merge_file_a20576
         collection.find({}).toArray((error, result) => {
             if(error) {
                 return response.status(500).send(error);
@@ -40,3 +61,67 @@ module.exports = function(app) {
         });
     });
 };
+=======
+        var query = User.find({});
+        query.exec(function(err, users){
+            if(err)
+                res.send(err);
+
+            // If no errors are found, it responds with a JSON of all users
+            res.json(users);
+        });
+    });
+
+    // GET Routes
+    // --------------------------------------------------------
+    // Retrieve records for all users in the db
+    /*app.get('/crimes', function(req, res){
+
+        // Uses Mongoose schema to run the search (empty conditions)
+        var query = User.find({});
+        query.exec(function(err, users){
+            if(err)
+                res.send(err);
+
+            // If no errors are found, it responds with a JSON of all users
+            res.json(users);
+        });
+    });*/
+
+    // POST Routes
+    // --------------------------------------------------------
+    // Provides method for saving new users in the db
+    app.post('/users', function(req, res){
+
+        // Creates a new User based on the Mongoose schema and the post bo.dy
+        var newuser = new User(req.body);
+
+        // New User is saved in the db.
+        newuser.save(function(err){
+            if(err)
+                res.send(err);
+
+            // If no errors are found, it responds with a JSON of the new user
+            res.json(req.body);
+        });
+    });
+/*
+    // POST Routes
+    // --------------------------------------------------------
+    // Provides method for saving new users in the db
+    app.post('/crimes', function(req, res){
+
+        // Creates a new User based on the Mongoose schema and the post bo.dy
+        var newuser = new User(req.body);
+
+        // New User is saved in the db.
+        newuser.save(function(err){
+            if(err)
+                res.send(err);
+
+            // If no errors are found, it responds with a JSON of the new user
+            res.json(req.body);
+        });
+    });*/
+};
+>>>>>>> .merge_file_a14240
