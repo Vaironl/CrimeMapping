4a1c9477755f609e06647e3ae919cf6dc9667496
nmendoza <vmend001@odu.edu>
2019-03-04 12:05:06 -0500

    Preparing to merge with working.

--- dist/mapLoad.js
+++ dist/mapLoad.js
@@ -1,7 +1,12 @@
 "use strict"; // Creates the mapLoad Module and Controller. Note that it depends on the 'geolocation' module and service.
 
 var mapLoad = angular.module('mapLoad', ['gservice']);
-mapLoad.controller('mapLoad', function ($scope, $http, gservice) {
-  google.maps.event.addDomListener(window, 'load', gservice.refresh());
+mapLoad.controller('mapLoad', function ($scope, $window, $http, gservice) {
+    google.maps.event.addDomListener(window, 'load', gservice.refresh());
+
+    $scope.publicProfileToggle = function () {
+        alert("public profile toggle");
+    };
+
 });
 //# sourceMappingURL=mapLoad.js.map
