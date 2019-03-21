885b8e734aec4c1a7f6c7e803dd3b0e675c61ac9
Kevin Chahine <kchah001@odu.edu>
2019-03-21 15:10:50 -0400

    DistanceFactor class is ready to use. See ./app/Modules/DistanceFactor.js for instructions

--- app/Modules/DistanceFactor.js
+++ app/Modules/DistanceFactor.js
@@ -1,20 +1,62 @@
-/** Class rep
- * */
-class DistanceFactor {
+/**
+ * Class DistanceFactor
+ *
+ * @description:
+ * Purpose:
+ *      Used to calculate the impact (Crime SafetyScore) that a crime plays on any point
+ *      on the map. This calculation involves the distance of the crime from the
+ *      arbitrary point. The further the crime is from the point, the less impact it has
+ *      on the SafetyScore of that point. Crimes committed at the point will have an impact
+ *      of 1.0. As the distance increases the limit of the impact approaches 0.0.
+ *
+ * @example:
+ *      Say a crime with a severity of 4 is committed a distance of 100 units away from an
+ *      arbitrary point. The distance factor can be calculated as follows:
+ *
+ *          var distFac = DistanceFactor.calculateDistanceFactor(100);
+ *
+ *      This will give a value between 0.0 and 1.0.
+ *
+ *      The impact of the crime on that point can be calculated as follows:
+ *
+ *          impact = DistanceFactor.calculateDistanceFactor(100) * 4;
+ *
+ *      Note: (4.0 > impact >= 0.0)
+ *
+ * @description:
+ * Note:
+ *      The DistanceFactor can only be applied to a single point and a single Crime. To calculate
+ *      the SafetyScore of multiple crimes Use the SafetyScore class.
+ *
+ * @description
+ * How it works:
+ *      The DistanceFactor can be calculated in one of 3 ways:
+ *          - linear distance
+ *          - inverse distance
+ *          - bell curve distance (probably the most effective)
+ *
+ *      For descriptions of each way, see the functions inside class DistanceFactor
+ *      See link for a graphical representation of the distance factor
+ *      @see https://www.desmos.com/calculator/qosbo4ty35
+ *
+ */
+export default class DistanceFactor {
     /**
      * @description Calculates the distance factor between a single Crime and the any single point on the map.
      *              Generally the further the crime from the point, the lower the distance factor.
      *              Crimes infinitely far from the point will always have a distance factor of 0 or an extremely small number.
      *              Crimes located at the point will always have a distance factor of 1
      *
-     *              Crimes in between will be between 0 and 1 with a trend of either linear, inverse or bell curve
-     *                  depending on the calculation used in function. See below functions.
+     *              Crimes in between will be between 0.0 and 1.0 with a trend of either linear, inverse or bell curve
+     *                  depending on the calculation used in the function. See below functions.
      *
-     *              This function is used to calculate the Crime SafteyScore of a point on the map
+     *              This function is used to help calculate the Crime SafteyScore of a point on the map
      *
      * @param {Crime} crime     - crime we are calculating the weight of
      * @param {Point} point     - point that we want to calculate the SafetyScore of
-     * @returns {Number}        - result will be between 0 and 1
+     *                              coordinates of point must be lat and lng in units of
+     *                              degrees. (x -> lat, y ->lng)
+     * @returns {Number}        - distance factor. will be between 0.0 and 1.0
      *
      * See link for a graphical representation of the distance factor
      * @see https://www.desmos.com/calculator/qosbo4ty35
@@ -22,7 +64,93 @@ class DistanceFactor {
     static calcDistanceFactor(crime, point) {
         let angleFromCrime = this.calcAngleBetween(crime.lat, crime.lng, point.x, point.y);
 
-        return calcBellCurveDistanceFactor(distanceFromCrime);
+        return DistanceFactor.calcBellCurveDistanceFactor(angleFromCrime);
+    }
+
+    /**
+     * @private
+     *
+     * @description Calculates the distance factor of a crime based on the linear distance
+     * Crimes with a distance of 0 degrees have a factor of 1.0.
+     * Crimes with a distance of cutoffDistance or greater have a factor of 0.0.
+     * Crimes in between have a factor of -distanceFromCrime / cutoffDistance + 1.
+     *
+     * @param {Number} distanceFromCrime -  Distance that a point is from a crime. (Make sure this is >= 0)
+     *                                      Must be in units of degrees.
+     * @param {Number} cutoffDistance -     Max distance that is relevant. Distances greater than
+     *                                      cutoffDistance have a factor of 0.0
+     *                                      Must be in units of degrees.
+     * @returns {Number}                    distance factor : result will be between 0.0 and 1.0 degrees
+     * @see https://www.desmos.com/calculator/swjdvtkohs
+     */
+    static calcLinearDistanceFactor(distanceFromCrime, cutoffDistance) {
+        if (distanceFromCrime < cutoffDistance) {
+            return -distanceFromCrime / cutoffDistance + 1
+        }
+        else {
+            return 0;
+        }
+    }
+
+    /**
+     * @private
+     *
+     * @discription Calculates the distance factor of a crime based on the inverse of the distance.
+     * Crimes with a distance of 0 have a factor of 1
+     * Crimes >= 0 have a factor of 1 / (distanceFromCrime + 1)
+     *
+     * @param {number} distanceFromCrime    Distance that a point is from a crime. (Make sure this is >= 0)
+     *                                      Should be in units of degrees.
+     * @returns {number}                    distance factor : result will be between 0 and 1
+     * @see https://www.desmos.com/calculator/f6ob5hmn3w
+     */
+    static calcInverseDistanceFactor(distanceFromCrime) {
+        return 1 / (distanceFromCrime + 1);
+    }
+
+    /**
+     * @private
+     *
+     * @description Calculates the distance factor of a crime based on the bell curve of the distance.
+     * Crimes with a distance of 0 degrees have a factor of 1.0
+     * Crimes with a greater distance have a factor in the shape of a bell curve.
+     * As the distance increases the distance factors approaches 0.0
+     *
+     * @param {Number} distanceFromCrime    Distance that a point is from a crime. (Make sure this is >= 0)
+     *                                      Must be in units of degrees.
+     * @returns {Number} distanceFactor     distance factor : result will be between 0.0 and 1.0
+     *
+     * @see https://www.desmos.com/calculator/c3atr11svh
+     */
+    static calcBellCurveDistanceFactor(distanceFromCrime) {
+        let sigma = 1.0;    // The larger sigma the flatter the curve
+
+        return DistanceFactor.bellCurveFunction(distanceFromCrime, sigma);
+    }
+
+    /**
+     * @private
+     *
+     * @description calculates the bell curve function and scales it up or down so that
+     * the height of the curve is 1.0 when x = 0.0 no matter the value sigma.
+     *
+     * See the red curve in:
+     * @see https://www.desmos.com/calculator/qosbo4ty35
+     *
+     * @param x {number}
+     * @param sigma {number}
+     * @returns {number}
+     */
+    static bellCurveFunction(x, sigma) {
+        let power = -(x ** 2.0) / (2.0 * sigma * sigma);
+
+        let coefficient = 1.0 / (sigma * Math.sqrt(2.0 * Math.PI));
+
+        let bell = coefficient * Math.E ** power;
+
+        let bellCurve = bell / coefficient;
+
+        return bellCurve;
     }
 
     /**
@@ -64,75 +192,6 @@ class DistanceFactor {
 
         // Convert result back to degrees and return
         return this.radToDeg(Math.acos(dotProduct));
-    }
-
-    /**
-     * @description Calculates the distance factor of a crime based on the linear distance
-     * Crimes with a distance of 0 have a factor of 1.
-     * Crimes with a distance of cutoffDistance or greater have a factor of 0.
-     * Crimes in between have a factor of -distanceFromCrime / cutoffDistance + 1.
-     *
-     * @param {Number} distanceFromCrime -  Distance that a point is from a crime. (Make sure this is >= 0)
-     *                                      Should be in units of degrees.
-     * @param {Number} cutoffDistance -     Max distance that is relevant. Distances greater than
-     *                                      cutoffDistance have a factor of 0
-     *                                      Should be in units of degrees.
-     * @returns {Number}                    distance factor : result will be between 0 and 1 degrees
-     * @see https://www.desmos.com/calculator/swjdvtkohs
-     */
-    static calcLinearDistanceFactor(distanceFromCrime, cutoffDistance) {
-        if (distanceFromCrime < cutoffDistance) {
-            return -distanceFromCrime / cutoffDistance + 1
-        }
-        else {
-            return 0;
-        }
-    }
-
-    /**
-     * @discription Calculates the distance factor of a crime based on the inverse of the distance.
-     * Crimes with a distance of 0 have a factor of 1
-     * Crimes >= 0 have a factor of 1 / (distanceFromCrime + 1)
-     *
-     * @param {number} distanceFromCrime    Distance that a point is from a crime. (Make sure this is >= 0)
-     *                                      Should be in units of degrees.
-     * @returns {number}                    distance factor : result will be between 0 and 1
-     * @see https://www.desmos.com/calculator/f6ob5hmn3w
-     */
-    static calcInverseDistanceFactor(distanceFromCrime) {
-        return 1 / (distanceFromCrime + 1);
-    }
-
-    /**
-     * @description Calculates the distance factor of a crime based on the bell curve of the distance.
-     * Crimes with a distance of 0 have a factor of 1
-     * Crimes with a greater distance have a factor in the shape of a bell curve
-     *
-     * @param {Number} distanceFromCrime    Distance that a point is from a crime. (Make sure this is >= 0)
-     *                                      Should be in units of degrees.
-     * @returns {Number} distanceFactor     distance factor : result will be between 0 and 1
-     *
-     * @see https://www.desmos.com/calculator/c3atr11svh
-     */
-    static calcBellCurveDistanceFactor(distanceFromCrime) {
-        let sigma = 1.0;    // The larger sigma the flatter the curve
-
-        let power = -(distanceFromCrime ** 2) / (2 * sigma * sigma);
-
-        let coefficient = 1 / (sigma * Math.sqrt(2 * Math.PI));
-
-        let distanceFactor = coefficient * Math.E ** power;
-
-        // Now the peak of the bell curve will probably not be at 1
-        // So we need to scale the curve so that it will equal 1 at the peak (when distance equals 0)
-        let scalingFactorPower = 0;
-
-        let scalingFactorCoefficient = 1 / (sigma * Math.sqrt(2 * Math.PI));
-
-        let scalingFactor = scalingFactorCoefficient * Math.E ** scalingFactorPower;
-        scalingFactor = 1 / scalingFactor;
-
-        return scalingFactor * distanceFactor;
     }
 
     /**
